12eaf758bf53fc3e3b0199a83a47f65ad771d079
ABC <vinayaksh8432@gmail.com>
2025-02-20 14:22:28 +0530

    Lec-7

---
+++ Lec-8_EmailVerification/utils/sendMail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = (ElementInternals, subject, message) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "vs1234676@gmail.com",
+            pass: "tjfl dnto ezkj xwfz",
+        },
+    });
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+            from: "vs1234676@gmail.com", // sender address
+            to: "vs1234676@gmail.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+    }
+
+    main().catch(console.error);
+};
+
+module.exports.sendMail - sendMail;
