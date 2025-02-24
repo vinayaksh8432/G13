8417b6313eb0eff8c8de3010f3785f923fce701d
ABC <vinayaksh8432@gmail.com>
2025-02-24 15:43:52 +0530

    24thFeb

---
+++ Lec-9_Typescript/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let a = 10;
+console.log(a);
+let str = "Hello";
+console.log(str);
+let b = 20;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(a, b));
---
+++ Lec-9_Typescript/index.ts
@@ -0,0 +1,9 @@
+let a: number = 10;
+console.log(a);
+let str: string = "Hello";
+console.log(str);
+let b: number = 20;
+function sum(a: number, b: number): number {
+    return a + b;
+}
+console.log(sum(a, b));
